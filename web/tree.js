17dcc0a3ee28e8414c304e58594cbaa943dbd4ab
Alex Nichol <unixpickle@gmail.com>
2023-03-19 14:57:50 -0700

    start working on ray casting

--- web/tree.js
+++ web/tree.js
@@ -7,6 +7,10 @@
             this.z = z;
         }
 
+        static zero() {
+            return new Vector(0, 0, 0);
+        }
+
         static axis(idx) {
             if (idx === 0) {
                 return new Vector(1, 0, 0);
@@ -23,6 +27,18 @@
             return this.x * v1.x + this.y * v1.y + this.z * v1.z;
         }
 
+        norm() {
+            return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+        }
+
+        scale(s) {
+            return new Vector(this.x * s, this.y * s, this.z * s);
+        }
+
+        add(v) {
+            return new Vector(this.x + v.x, this.y + v.y, this.z + v.z);
+        }
+
         getAxis(axis) {
             if (axis === 0) {
                 return this.x;
@@ -36,6 +52,25 @@
         }
     }
 
+    class Ray {
+        constructor(origin, direction) {
+            this.origin = origin;
+            this.direction = direction;
+        }
+
+        at(t) {
+            return this.origin.add(this.direction.scale(t));
+        }
+    }
+
+    class ChangePoint {
+        constructor(point, normal, t) {
+            this.point = point;
+            this.normal = normal;
+            this.t = t;
+        }
+    }
+
     class Tree {
         constructor(axis, threshold, left, right, leaf) {
             this.axis = axis;
@@ -45,8 +80,70 @@
             this.leaf = leaf;
         }
 
-        static leaf(value) {
+        static newLeaf(value) {
             return new Tree(null, null, null, null, value);
+        }
+
+        isLeaf() {
+            return this.axis !== null;
+        }
+
+        nextChange(ray) {
+            if (this.isLeaf()) {
+                return null;
+            }
+            const dirDot = ray.direction.dot(this.axis);
+            const absDirDot = Math.abs(dirDot);
+            if (absDirDot < this.axis.norm() * ray.direction.norm() * 1e-8) {
+                return null;
+            }
+
+            const curDot = this.axis.dot(ray.direction);
+            const child = curDot >= this.threshold ? this.right : this.left;
+            const normal = child === this.right ? this.axis : this.axis.scale(-1);
+            const thisT = (this.threshold - curDot) / dirDot;
+
+            // This edge case might seem extremely unusual, but it actually occurs
+            // naturally for trees with tight bounding boxes.
+            if (this.threshold == curDot) {
+                const maxT = 1e8;
+                const maxDot = this.axis.dot(ray.at(maxT));
+                if ((curDot >= this.threshold) != (maxDot >= this.threshold)) {
+                    const changeT = this._changeT(ray, thisT, maxT);
+                    return new ChangePoint(ray.at(changeT), normal, changeT);
+                }
+            }
+
+            const childRes = child.nextChange(ray);
+            if (thisT <= 0) {
+                return childRes;
+            } else if (childRes !== null && thisT > childRes.t) {
+                return childRes;
+            } else {
+                const changeT = this._changeT(origin, direction, thisT, thisT * 2);
+                return new ChangePoint(ray.at(changeT), normal, changeT);
+            }
+        }
+
+        _changeT(ray, minT, maxT) {
+            const orig = this.axis.dot(ray.origin) < this.threshold;
+            const x = ray.at(minT);
+            if (this.axis.dot(x) < this.threshold != orig) {
+                return minT;
+            }
+            if (this.axis.dot(ray.at(maxT)) < this.threshold == orig) {
+                console.error("impossible situation encountered: collision was expected");
+                return maxT;
+            }
+            for (let i = 0; i < 32; ++i) {
+                const midT = (minT + maxT) / 2;
+                if (this.axis.dot(ray.at(midT)) < this.threshold != orig) {
+                    maxT = midT;
+                } else {
+                    minT = midT;
+                }
+            }
+            return maxT;
         }
     }
 
@@ -106,8 +203,8 @@
         // Apply bounds as branches of the tree.
         for (let axis = 0; axis < 3; ++axis) {
             const ax = Vector.axis(axis);
-            tree = new Tree(ax, min.getAxis(axis), Tree.leaf(false), tree);
-            tree = new Tree(ax, max.getAxis(axis), tree, Tree.leaf(false));
+            tree = new Tree(ax, min.getAxis(axis), Tree.newLeaf(false), tree);
+            tree = new Tree(ax, max.getAxis(axis), tree, Tree.newLeaf(false));
         }
         return tree;
     }
