41dcc2adeb360d8cd7d91d455e5d8dedacdc185f
Alex Nichol <unixpickle@gmail.com>
2023-03-26 13:44:09 -0700

    js matrix primitive

--- web/types.js
+++ web/types.js
@@ -74,6 +74,103 @@
             // Avoid exception for optimization.
             return 0;
         }
+
+        orthoBasis() {
+            const absX = Math.abs(this.x);
+            const absY = Math.abs(this.y);
+            const absZ = Math.abs(this.z);
+
+            // Create the first basis vector by swapping two
+            // coordinates and negating one of them.
+            // For numerical stability, we involve the component
+            // with the largest absolute value.
+            let basis1 = Vector.zero();
+            if (absX > absY && absX > absZ) {
+                basis1.x = this.y / absX;
+                basis1.y = -this.x / absX;
+            } else {
+                basis1.y = this.z;
+                basis1.z = -this.y;
+                if (absY > absZ) {
+                    basis1.y /= absY;
+                    basis1.z /= absY;
+                } else {
+                    basis1.y /= absZ;
+                    basis1.z /= absZ;
+                }
+            }
+
+            // Create the second basis vector using a cross product.
+            const basis2 = new Vector(
+                basis1.y * this.z - basis1.z * this.y,
+                basis1.z * this.x - basis1.x * this.z,
+                basis1.x * this.y - basis1.y * this.x,
+            ).normalize();
+            basis1 = basis1.normalize();
+            const basis0 = this.normalize();
+
+            return new Matrix(
+                basis0.x, basis1.x, basis2.x,
+                basis0.y, basis1.y, basis2.y,
+                basis0.z, basis1.z, basis2.z,
+            );
+        }
+    }
+
+    class Matrix {
+        constructor(a, b, c, d, e, f, g, h, i) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.e = e;
+            this.f = f;
+            this.g = g;
+            this.h = h;
+            this.i = i;
+        }
+
+        static rotation(v, theta) {
+            const basis = v.orthoBasis();
+            const raw = new Matrix(
+                1, 0, 0,
+                0, Math.cos(theta), -Math.sin(theta),
+                0, Math.sin(theta), Math.cos(theta),
+            );
+            return basis.mul(raw.mul(basis.t()));
+        }
+
+        apply(v) {
+            return new Vector(
+                this.a * v.x + this.b * v.y + this.c * v.z,
+                this.d * v.x + this.e * v.y + this.f * v.z,
+                this.g * v.x + this.h * v.y + this.i * v.z,
+            );
+        }
+
+        mul(m) {
+            return new Matrix(
+                this.a * m.a + this.b * m.d + this.c * m.g,
+                this.a * m.b + this.b * m.e + this.c * m.h,
+                this.a * m.c + this.b * m.f + this.c * m.i,
+
+                this.d * m.a + this.e * m.d + this.f * m.g,
+                this.d * m.b + this.e * m.e + this.f * m.h,
+                this.d * m.c + this.e * m.f + this.f * m.i,
+
+                this.g * m.a + this.h * m.d + this.i * m.g,
+                this.g * m.b + this.h * m.e + this.i * m.h,
+                this.g * m.c + this.h * m.f + this.i * m.i,
+            );
+        }
+
+        t() {
+            return new Matrix(
+                this.a, this.d, this.g,
+                this.b, this.e, this.h,
+                this.c, this.f, this.i,
+            );
+        }
     }
 
     class Ray {
