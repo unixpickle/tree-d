139b56822b5adef68631f5f204197ce0e53e80f1
Alex Nichol <unixpickle@gmail.com>
2023-04-06 21:38:35 -0700

    add more models

--- web/model_registry.js
+++ web/model_registry.js
@@ -2,16 +2,20 @@ const Matrix = self.treed.Matrix;
 const Vector = self.treed.Vector;
 
 self.treed.models = [
-    // {
-    //     name: 'Moai',
-    //     source: 'https://www.thingiverse.com/thing:2493386',
-    //     initMatrix: Matrix.rotation(new Vector(0, 0, 1), Math.PI),
-    // },
-    // {
-    //     name: 'Corgi',
-    //     source: 'https://www.thingiverse.com/thing:2806745',
-    //     initMatrix: Matrix.rotation(new Vector(0, 0, 1), Math.PI / 2),
-    // },
+    {
+        name: 'Moai',
+        path: 'data/moai',
+        source: 'https://www.thingiverse.com/thing:2493386',
+        initMatrix: Matrix.rotation(new Vector(0, 0, 1), Math.PI),
+        metadata: { "normals": { "num_leaves": 10933, "filename": "normals.bin", "file_size": 437304 }, "lods": [{ "num_leaves": 5805, "filename": "full.bin", "file_size": 185768 }, { "num_leaves": 4096, "filename": "lod_4096.bin", "file_size": 131080 }, { "num_leaves": 2047, "filename": "lod_2047.bin", "file_size": 65512 }, { "num_leaves": 1024, "filename": "lod_1024.bin", "file_size": 32776 }, { "num_leaves": 512, "filename": "lod_512.bin", "file_size": 16392 }, { "num_leaves": 256, "filename": "lod_256.bin", "file_size": 8200 }] },
+    },
+    {
+        name: 'Corgi',
+        path: 'data/corgi',
+        source: 'https://www.thingiverse.com/thing:2806745',
+        initMatrix: Matrix.rotation(new Vector(0, 0, 1), Math.PI / 2),
+        metadata: { "normals": { "num_leaves": 6739, "filename": "normals.bin", "file_size": 269544 }, "lods": [{ "num_leaves": 5130, "filename": "full.bin", "file_size": 164168 }, { "num_leaves": 4095, "filename": "lod_4095.bin", "file_size": 131048 }, { "num_leaves": 2048, "filename": "lod_2048.bin", "file_size": 65544 }, { "num_leaves": 1023, "filename": "lod_1023.bin", "file_size": 32744 }, { "num_leaves": 512, "filename": "lod_512.bin", "file_size": 16392 }, { "num_leaves": 256, "filename": "lod_256.bin", "file_size": 8200 }] },
+    },
     {
         name: 'Curvy Thing',
         path: 'data/curvy_thing',
