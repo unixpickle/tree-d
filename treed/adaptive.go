63c396e75f5a8ba13840d5451e9c7b61a78851f3
Alex Nichol <unixpickle@gmail.com>
2023-04-10 21:22:23 -0700

    greedy branch

--- treed/adaptive.go
+++ treed/adaptive.go
@@ -1,6 +1,11 @@
 package treed
 
-import "golang.org/x/exp/constraints"
+import (
+	"math"
+
+	"github.com/unixpickle/essentials"
+	"golang.org/x/exp/constraints"
+)
 
 const DefaultAdaptiveGreedyTreeSampleIters = 20
 
@@ -23,4 +28,41 @@ func AdaptiveGreedyTree[F constraints.Float, C Coord[F, C], T any](
 	maxDepth int,
 ) *Tree[F, C, T] {
 	panic("not yet implemented")
+}
+
+func greedySearchSingle[F constraints.Float, C Coord[F, C], T any](
+	axes []C,
+	coords []C,
+	targets []T,
+	loss SplitLoss[F, T],
+	concurrency int,
+) splitResult {
+	best := splitResult{SplitInfo: SplitInfo{Loss: math.Inf(1)}}
+	essentials.ReduceConcurrentMap(concurrency, len(axes), func() (func(i int), func()) {
+		localBest := splitResult{SplitInfo: SplitInfo{Loss: math.Inf(1)}}
+
+		localCoords := append([]C{}, coords...)
+		localTargets := append([]T{}, targets...)
+		localDots := make([]F, len(coords))
+		process := func(i int) {
+			axis := axes[i]
+			for j, x := range localCoords {
+				localDots[j] = x.Dot(axis)
+			}
+			essentials.VoodooSort(localDots, func(i, j int) bool {
+				return localDots[i] < localDots[j]
+			}, localTargets, localCoords)
+			res := loss.MinimumSplit(NewListSlice(localTargets), NewListSlice(localDots))
+			if res.Loss < localBest.Loss {
+				localBest = splitResult{SplitInfo: res, Axis: i}
+			}
+		}
+		reduce := func() {
+			if localBest.Loss < best.Loss {
+				best = localBest
+			}
+		}
+		return process, reduce
+	})
+	return best
 }
